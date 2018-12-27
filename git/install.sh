2822d23a29a11b0f20ea898987bab81ecf75b006
Dennis Evert <dennisevert@gmail.com>
2018-12-27 16:07:35 -0600

    Add ssh config file that can be symlinked for ssh agent key storage

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,6 @@
 # Symlink .gitconfig and .gitignore_global
 sudo ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 sudo ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+
+# Symlink ssh config
+sudo ln -s ~/.dotfiles/git/.ssh_config ~/.ssh/config
