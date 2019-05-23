fd5fdf3865acd9b4eee8cb0bfb737d16e3d8129a
Dennis Evert <dennisevert@gmail.com>
2019-05-23 10:08:28 -0500

    Custom keybinding setting for fish

--- fish/install.sh
+++ fish/install.sh
@@ -10,6 +10,7 @@ curl -L https://get.oh-my.fish | fish
 mkdir -p ~/.config/fish/functions
 sudo ln -s /usr/local/share/gitprompt.fish ~/.config/fish/functions/fish_prompt.fish
 sudo ln -s ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
+sudo ln -s ~/.dotfiles/fish/fish_user_key_bindings.fish ~/.config/fish/functions/fish_user_key_bindings.fish
 
 # Install Oh My Fish Packages
 omf install bass
