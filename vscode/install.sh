2e04707c6b2ba0850d7035447933d46086e4b986
Dennis Evert <dennisevert@gmail.com>
2019-06-14 13:24:24 -0500

    Add custom javascript snippets for VSCode

--- vscode/install.sh
+++ vscode/install.sh
@@ -65,21 +65,25 @@ if [[ $? -eq 0 ]];then
         echo "Skipping extension install.";
     fi
 
-    read -r -p "Do you want to create symlinks to user config and keybindings? [y|N] " configresponse
+    read -r -p "Do you want to create symlinks to user config, keybindings and snippets? [y|N] " configresponse
     if [[ $configresponse =~ ^(y|yes|Y) ]];then
-        read -r -p "Do you want to back up your current user config and keybindings? [Y|n] " backupresponse
+        read -r -p "Do you want to back up your current user config, keybindings and snippets? [Y|n] " backupresponse
         if [[ $backupresponse =~ ^(n|no|N) ]];then
-            echo "Skipping user config and keybindings backup."
+            echo "Skipping user config, keybindings and snippets backup."
         else
             cp $HOME/Library/Application\ Support/Code/User/settings.json $HOME/Library/Application\ Support/Code/User/settings.backup.json
             echo "Your previous config has been saved to: $HOME/Library/Application Support/Code/User/settings.backup.json"
             cp $HOME/Library/Application\ Support/Code/User/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.backup.json
             echo "Your previous keybindings have been saved to: $HOME/Library/Application Support/Code/User/keybindings.backup.json"
+            cp -R $HOME/Library/Application\ Support/Code/User/snippets $HOME/Library/Application\ Support/Code/User/snippets.backup
+            echo "Your previous snippets have been saved to: $HOME/Library/Application Support/Code/User/snippets.backup"
         fi
         rm -v $HOME/Library/Application\ Support/Code/User/settings.json
         sudo ln -s $HOME/.dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
         rm -v $HOME/Library/Application\ Support/Code/User/keybindings.json
         sudo ln -s $HOME/.dotfiles/vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+        rm -rf $HOME/Library/Application\ Support/Code/User/snippets
+        sudo ln -s $HOME/.dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code/User/snippets
 
         echo "New user config and keybindings have been written. Please restart VSCode."
     else
