4b86fc1985b6e7fbccb355f56428bf671cbecdf8
Dennis Evert <dennisevert@gmail.com>
2024-03-29 20:00:07 -0500

    Formatting lua files for nvim config and fixing tree-sitter config

--- neovim/lua/plugins/lsp-config.lua
+++ neovim/lua/plugins/lsp-config.lua
@@ -8,9 +8,9 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-       require("mason-lspconfig").setup({
-         ensure_installed = { "lua_ls" }
-       })
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
     end
   },
   {
@@ -44,10 +44,10 @@ return {
           vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
           vim.keymap.set('n', '<space>f', function()
-          vim.lsp.buf.format { async = true }
-      end, opts)
-  end,
-})
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
     end
   }
 }
--- neovim/lua/plugins/tree-sitter.lua
+++ neovim/lua/plugins/tree-sitter.lua
@@ -1,16 +1,14 @@
 return {
-  require("lazy").setup({{
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-      local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "javascript", "html" },
-          sync_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-    end
-  }})
- }
+    configs.setup({
+      ensure_installed = { "lua", "javascript", "html" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end
+}
