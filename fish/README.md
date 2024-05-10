ce8e8a095d8340252178da74050d6bd70c0b58c4
Dennis Evert <dennisevert@gmail.com>
2024-05-10 11:10:04 -0500

    So long, and thanks for all the fish

--- fish/README.md
+++
@@ -1,7 +0,0 @@
-# Fish Setup
-
-Switch to using brew install fish as default shell
-
-```
-chsh -s /usr/local/bin/fish
-```
--- fish/fish_user_key_bindings.fish
+++
@@ -1,7 +0,0 @@
-function fish_user_key_bindings
-  fish_default_key_bindings
-  fzf_key_bindings
-
-  # Custom Keybindings
-  # bind \t accept-autosuggestion     # Tab key will accept command suggestions
-end
