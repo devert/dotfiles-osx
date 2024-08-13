edb50198beedfb1aadf3788de2ad6e6561e49dd9
Dennis Evert <dennisevert@gmail.com>
2024-08-13 09:49:47 -0500

    Disable homebrew auto update in ZSH exports

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,3 +1,6 @@
+# Homebrew will not auto-update before running `brew install`,`brew upgrade` or `brew tap`.
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 # Puppeteer installs will skip Chromium install and use Homebrew installed Chromium
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 export PUPPETEER_EXECUTABLE_PATH=`which chromium`
