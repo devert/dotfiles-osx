2e722aa4179797a7278753e70744d25efa98db0e
Dennis Evert <dennisevert@gmail.com>
2025-01-07 23:56:59 -0600

    Configure FZF colors

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,3 +1,12 @@
+# FZF Default Options
+export FZF_DEFAULT_OPTS="--prompt='❯ '"
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+  --color=fg:-1,fg+:-1,bg:-1,bg+:#373539
+  --color=hl:#78dce8,hl+:#ff6188,info:#727072,marker:#fc9867
+  --color=prompt:#a9dc76,spinner:#fc9867,pointer:#ff6188,header:#727072
+  --color=border:#727072,label:#fcfcfa,query:#fcfcfa
+  --info=right'
+
 # Homebrew will not auto-update before running `brew install`,`brew upgrade` or `brew tap`.
 export HOMEBREW_NO_AUTO_UPDATE=1
 
