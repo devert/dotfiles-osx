70a912f7c409b0a8560ca77b8914682ab0775bdc
Dennis Evert <dennisevert@gmail.com>
2025-11-03 23:05:00 -0600

    Symlink leader-key config properly

--- leader-key/install.sh
+++ leader-key/install.sh
@@ -2,5 +2,4 @@
 
 echo "Configuring Leader Key"
 
-mkdir $HOME/.config/leader-key
-sudo ln -s $HOME/.dotfiles/leader-key/config.json $HOME/.config/leader-key
+sudo ln -s $HOME/.dotfiles/leader-key $HOME/.config
