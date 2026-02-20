8957e0f44254eeae7ec9f8905d601089bd8ba51a
Dennis Evert <devert@mac.lan>
2026-02-20 11:19:58 -0600

    Tahoe changes for finder settings

--- macos/install.sh
+++ macos/install.sh
@@ -130,15 +130,6 @@ defaults write NSGlobalDomain com.apple.sound.beep.volume -int 0
 defaults write NSGlobalDomain com.apple.sound.uiaudio.enabled -int 0 
 
 
-
-###############################################################################
-#
-# TODO: Set and verify all the below settings on MacOS Tahoe
-#
-###############################################################################
-
-
-
 ###############################################################################
 # Finder
 ###############################################################################
@@ -156,6 +147,23 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Disable the warning before emptying the Trash
+# defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+
+
+###############################################################################
+#
+# TODO: Set and verify all the below settings on MacOS Tahoe
+#
+###############################################################################
+
+
+
 # Enable snap-to-grid for icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
@@ -170,19 +178,6 @@ defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 64" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 64" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 64" ~/Library/Preferences/com.apple.finder.plist
-
-# Show the ~/Library folder
-chflags nohidden ~/Library
-
-# Show the /Volumes folder
-sudo chflags nohidden /Volumes
-
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-# Disable the warning before emptying the Trash
-defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 
 ###############################################################################
