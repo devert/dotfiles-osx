6d79a9d4c19d5739d5635e70ae0a3bc8d90efd43
Dennis Evert <dennisevert@gmail.com>
2026-02-24 15:05:33 -0600

    Updated Leader Key configuration and install script

--- leader-key/install.sh
+++ leader-key/install.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env zsh
 
 # Install Karabiner-Elements 
-echo "Installing Leader Key"
+echo "Installing Leader Key..."
 
 brew install leader-key
 
 # Configure Leader Key
-echo "Configuring Leader Key"
+echo "Configuring Leader Key..."
 
-export LK_CONFIG_DIR="$HOME/Library/Application Support/Leader Key"
-export LK_CONFIG_PATH="$LK_CONFIG_DIR/config.json"
+# Create a symlink so the entire Leader Key folder points to the dotfiles config
+export LEADER_KEY_CONFIG_DIR="$HOME/.config/leader-key"
+mkdir -p "$LEADER_KEY_CONFIG_DIR"
+ln -sfn "$HOME/.dotfiles/leader-key" "$LEADER_KEY_CONFIG_DIR"
 
-# If the existing Leader Key config exists, back it up so
-# we can replace it with a symlink to the dotfiles config.
-if [ -e "$LK_CONFIG_PATH" ] || [ -L "$LK_CONFIG_PATH" ]; then
-	timestamp=$(date +"%Y-%m-%d_%H%M%S")
-	echo "Backing up existing config to config.json.bak.$timestamp"
-	mv "$LK_CONFIG_PATH" "$LK_CONFIG_DIR/config.json.bak.$timestamp"
-fi
+# Additional Leader Key configuration using defaults write
+defaults write com.brnbw.Leader-Key "configDir" -string "$LEADER_KEY_CONFIG_DIR"
+defaults write com.brnbw.Leader-Key "KeyboardShortcuts_group-o" -string "{\"carbonModifiers\":6912,\"carbonKeyCode\":31}"
+defaults write com.brnbw.Leader-Key "KeyboardShortcuts_group-r" -string "{\"carbonKeyCode\":15,\"carbonModifiers\":6912}"
+defaults write com.brnbw.Leader-Key "KeyboardShortcuts_group-w" -string "{\"carbonModifiers\":6912,\"carbonKeyCode\":13}"
+defaults write com.brnbw.Leader-Key "KeyboardShortcuts_navigate" -string "{\"carbonKeyCode\":41,\"carbonModifiers\":6912}"
+defaults write com.brnbw.Leader-Key "groupShortcuts" -array "w" "o" "r"
+defaults write com.brnbw.Leader-Key "theme" -string breadcrumbs;
+defaults write com.brnbw.Leader-Key "screen" -string activeWindow
+defaults write com.brnbw.Leader-Key "modifierKeyConfiguration" -string "controlGroupOptionSticky";
+defaults write com.brnbw.Leader-Key "reactivateBehavior" -string hide;
 
-# Create a symlink so the entire leader-key folder points to the dotfiles config
-ln -sfn "$HOME/.dotfiles/leader-key/config.json" "$LK_CONFIG_PATH"
-
-echo "Leader Key Installation and Configuration Complete"
-echo "Go to Leader Key Preferences to Configure Your Shortcuts"
+echo "Leader Key installation and configuration complete!"
+echo "Restart Leader Key to apply changes"
