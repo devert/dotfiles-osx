1fe2605733907dc52de8312722e0ac6a8dd24406
Dennis Evert <dennisevert@gmail.com>
2026-02-23 23:10:49 -0600

    Updated git and github cli install and configs

--- git/install.sh
+++ git/install.sh
@@ -1,13 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+# Install Git and GitHub CLI 
+echo "Installing Git and GitHub CLI..."
+
+brew install git
+brew install gh
 
 # Symlink .gitconfig and .gitignore_global
-sudo ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-sudo ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
+echo "Installing Git configuration files..."
+
+sudo ln -sfn ~/.dotfiles/git/.gitconfig ~/.gitconfig
+sudo ln -sfn ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 
 # Configuration settings for Github CLI
+echo "Installing GitHub CLI configuration files..."
+
 mkdir ~/.config/gh
 sudo ln -s ~/.dotfiles/git/gh_config.yml ~/.config/gh/config.yml
 
 # Install some GitHub CLI extensions
-gh extension install github/gh-copilot
+echo "Installing GitHub CLI extensions..."
+
 gh extension install gennaro-tedesco/gh-f
+
+echo "Git and GitHub CLI Installation and Configuration Complete"
