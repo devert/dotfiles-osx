1bc49b5a42a4f3426a78cc4aba1d2e00b48f2387
Dennis Evert <dennisevert@gmail.com>
2026-02-25 13:37:36 -0600

    Updated yazi installation and config

--- yazi/install.sh
+++ yazi/install.sh
@@ -3,9 +3,12 @@
 echo "Installing Yazi..."
 brew install yazi
 
-echo "Configuring Yazi..."
+echo "Installing Yazi plugins..."
+sudo ya pkg add yazi-rs/plugins:smart-enter
 
+echo "Configuring Yazi..."
 sudo mkdir -p $HOME/.config/yazi
 sudo ln -sfn $HOME/.dotfiles/yazi/yazi.toml $HOME/.config/yazi/yazi.toml
+sudo ln -sfn $HOME/.dotfiles/yazi/keymap.toml $HOME/.config/yazi/keymap.toml
 
-echo "Yazi configuration complete!"
+echo "Yazi install and configuration complete!"
